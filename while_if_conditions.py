ede1bb98297f10cd4de2277acc864a04ccbc7bdc
Beto Wayne <47488555+falberto08@users.noreply.github.com>
2020-05-29 18:41:23 +0200

    Create while_if_conditions.py

---
+++ while_if_conditions.py
@@ -0,0 +1,32 @@
+lista = ["Hiderlano", "Luciano", "Euclides", "Aldeci", "Sousajr"]
+conta = "yes"
+print ("=" * 30)
+name = input("Type the first name: ")
+#while conta=="yes":
+while name in lista:
+            print(name.upper()," is a contact from Brazil. Analyze WhatsApp? (yes/no)")
+            resp = input()
+            if resp == "yes":
+                print("Fetching data... Press ENTER after 5s")
+                input()
+                if name == "Luciano":
+                    print("Boco'")
+                    break
+                else:
+                    if name == "Hiderlano":
+                        print("Panda")
+                        break
+                    else:
+                        if name == "Euclides":
+                            print("Pai")
+                            break
+                        else:
+                            if name == "Aldeci":
+                                print("highlander")
+                                break
+                            else:
+                                if name == "Sousajr":
+                                    print("Malhacao")
+                                    break
+else:
+    print(name.upper(), ' Is not a contact.')
